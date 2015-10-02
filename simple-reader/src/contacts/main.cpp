fa3f408e66e5ab0e94c426132a3bc82a57cf74a0
laolang2016 <xiaodaima2016@163.com>
2015-10-02 22:55:48 +0800

    完成： 	1、pugixml库的生成及测试

--- simple-reader/src/contacts/main.cpp
+++ simple-reader/src/contacts/main.cpp
@@ -7,10 +7,20 @@
 #include<iostream>
 
 #include"../../include/person.h"
-int main(void){
-	Person p("小代码","153","123@123.com");
+#include"../pugixml/pugixml.hpp"
+int main(void) {
+	
+	std::string path = "/home/laolang/git/simplereader/simple-reader/bin/";
+	std::string filename = path + "tree.xml";
+	
+	Person p("小代码", "153", "123@123.com");
 	std::cout << p.toString() << std::endl;
+	pugi::xml_document doc;
+
+	pugi::xml_parse_result result = doc.load_file(filename.c_str());
+
+	std::cout << "Load result: " << result.description() << ", mesh name: "
+			<< doc.child("mesh").attribute("name").value() << std::endl;
 	return 0;
 }
-
 
