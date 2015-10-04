94f525e9e640440728a5954dfc9aa79226c1374d
laolang2016 <xiaodaima2016@163.com>
2015-10-04 16:18:13 +0800

    完成： 	1、加入boost配置 	2、使用boost获取当前路径 	3、加入Makefile脚本,target列表： 		init:		--执行cmake,复制xml文件 		compile		--编译文件 		clean:		--清理 		execute:		--运行

--- simple-reader/src/contacts/main.cpp
+++ simple-reader/src/contacts/main.cpp
@@ -6,18 +6,42 @@
  */
 #include<iostream>
 
+#include<boost/filesystem.hpp>
+
 #include"../../include/person.h"
 #include"../pugixml/pugixml.hpp"
 #include"../../include/IPersonCrud.h"
 #include"../person/ImplPersonCrudPugixml.hpp"
-int main(void) {
 
-	std::string path = "/home/laolang/git/simplereader/simple-reader/bin/";
-	std::string filenametest = path + "tree.xml";
-	std::string filename = path + "person.xml";
+//using namespace boost::filesystem;
+namespace fs = boost::filesystem;
 
-	Person *p = new Person("小代码", "153", "123@123.com");
-	std::cout << p->toString() << std::endl;
+int main(int argc, char* argv[]) {
+	getcwd(NULL,0);
+
+//  //boost filesystem 测试
+//	if (argc < 2) {
+//		std::cout << "Usage: tut1 path\n";
+//		return 1;
+//	}
+//	std::cout << argv[1] << " " << fs::file_size(argv[1]) << '\n';
+////	std::string fullpath = fs::initial_path()<fs::path>().string();
+//	fs::path mypath;
+//	mypath /= argv[0];
+//	std::cout << "filename:" << mypath.filename() << std::endl;
+//	std::cout << "root path:" << mypath.root_path() << std::endl;
+
+//得到当前路径
+	string currentpath =
+			boost::filesystem::initial_path<boost::filesystem::path>().string();
+
+	std::string path =
+			"/home/laolang/git/simplereader/simplereader/simple-reader/bin/";
+
+//	std::string filenametest = path + "tree.xml";
+	std::string filenametest = currentpath + "/tree.xml";
+//	std::string filename = path + "person.xml";
+	std::string filename = currentpath + "/person.xml";
 
 	pugi::xml_document doc;
 
@@ -26,6 +50,9 @@ int main(void) {
 	std::cout << "Load result: " << result.description() << ", mesh name: "
 			<< doc.child("mesh").attribute("name").value() << std::endl;
 
+	Person *p = new Person("小代码", "153", "123@123.com");
+	std::cout << p->toString() << std::endl;
+	
 	ImplPersonCrudPugixml * pcPugi = new ImplPersonCrudPugixml(filename);
 	IPersonCrud * pc = pcPugi;
 	std::cout << "通过name查询：" << std::endl;
@@ -93,10 +120,10 @@ int main(void) {
 			it != personList->end(); ++it) {
 		std::cout << it->toString() << std::endl;
 	}
-	
+
 	std::cout << "通过给定的person属性添加：" << std::endl;
-	
-	pc->add("新二","000222","000222@qq.com");
+
+	pc->add("新二", "000222", "000222@qq.com");
 	std::cout << "输出所有：" << std::endl;
 	std::cout << "测试：size:" << personList->size() << std::endl;
 	personList = pc->selectAll();
